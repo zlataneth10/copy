74eb1492a49f1597568ea38674c662fbbad4b1da
kehinde cwb <85681159+codewithbillions@users.noreply.github.com>
2023-10-23 14:04:27 +0100

    quiz

---
+++ quiz-app/questions.js
@@ -0,0 +1,84 @@
+export default [
+    {
+        question: "who is the president of Nigeria?",
+        answers: [
+            {option: "Buhari", correct: false},
+            {option: "Obi", correct: false},
+            {option: "Tinubu", correct: true},
+        ]
+    },
+    {
+        question: "who is the most awarded African artist",
+        answers: [
+            {option: "Burnaboy", correct: false},
+            {option: "Wizkid", correct: true},
+            {option: "Davido", correct: false},
+        ]
+    },
+    {
+        question: "what is the largest city in Nigeria?",
+        answers: [
+            {option: "Benin", correct: true},
+            {option: "Kano", correct: false},
+            {option: "Ibadan", correct: true},
+        ]
+    },
+    {
+        question: "Babies are born without Knee cap",
+        answers: [
+            {option: "true", correct: true},
+            {option: "false", correct: false},
+            {option: "none of the above", correct: false},
+        ]
+    },
+
+    {
+        question: "who won the NBA finals for 2022 season?",
+        answers: [
+            {option: "Jamarront", correct: false},
+            {option: "Lebron James", correct: false},
+            {option: "Jovic", correct: true},
+        ]
+    },
+
+    {
+        question: "who is the current president of United State of America?",
+        answers: [
+            {option: "Goerge Bush", correct: false},
+            {option: "Osama bin laden", correct: false},
+            {option: "Joe Biden", correct: true},
+        ]
+    },
+    {
+        question: "who won the UEFA champion league 2022/2023",
+        answers: [
+            {option: "Real Madrid", correct: false},
+            {option: "Inter Milan", correct: false},
+            {option: "Man City", correct: true},
+        ]
+    },
+    {
+        question: "who won the 2022 FIFA world Cup",
+        answers: [
+            {option: "Portugal", correct: false},
+            {option: "Argentina", correct: true},
+            {option: "Brazil", correct: false},
+        ]
+    },
+    {
+        question: "where is the longest city called perth located?",
+        answers: [
+            {option: "Western Australia", correct: true},
+            {option: "West Africa", correct: false},
+            {option: "Belgium", correct: false},
+        ]
+    },
+    {
+        question: "what is the biggest Football Club in England?",
+        answers: [
+            {option: "Man City", correct: false},
+            {option: "Arsenal", correct: false},
+            {option: "Liverpool", correct: true},
+        ]
+    }
+]
